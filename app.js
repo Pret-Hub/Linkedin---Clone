13efed99e8df0f91b080bd0d3e3960e28cfec157
sed_lyf <pretamdas200@gmail.com>
2023-07-22 23:10:39 +0530

    first commit done!!!

---
+++ app.js
@@ -0,0 +1,25 @@
+
+let profileMenu = document.getElementById("profileMenu")
+
+function toggleMenu()
+{
+  profileMenu.classList.toggle("open-menu")
+}
+
+
+
+//-------------------to add show-more feature for smaller screen--------------------
+
+let sideActivity = document.getElementById("sidebarActivity")
+let moreLink = document.getElementById("showMoreLink")
+
+  function toggleActivity(){
+    sideActivity.classList.toggle("open-activity")
+
+    if(sideActivity.classList.contains("open-activity")){
+      moreLink.innerHTML = "Show less <b>-</b>"
+    }
+    else {
+      moreLink.innerHTML = "Show more <b>+</b>"
+    }
+  }
